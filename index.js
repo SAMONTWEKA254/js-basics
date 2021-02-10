6c362efad921a9b7690c56326d93cfba44c22a0a
Sam Ontweka <samsonotweka@zetech.ac.ke>
2021-02-10 14:24:11 +0300

    Adding index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    alert("Hello");
+    alert("How are you")
+}
+
+    function myFunction(){
+        let p=document.getElementById("greeting")
+       p.innerHTML="Hello Samson Ontweka";
+}
